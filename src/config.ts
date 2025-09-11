8f2a5300414a8bd01d6fc915a8620ea32fdac033
Abhishek Singh <87878794+3x10RaiseTo8@users.noreply.github.com>
2025-09-12 01:27:27 +0530

    Update SITE.website in config.ts

--- src/config.ts
+++ src/config.ts
@@ -90,7 +90,7 @@ export const SITE = {
   shortTitle: "OSD2025",
   description:
     "OpenSourceDay is all about giving back to the open source projects, sharpening your skills, and celebrating all things open source. Join us for a day!",
-  website: "https://opensourceweekend.org",
+  website: "https://osd.opensourceweekend.org",
   ogImage: "/images/OSWBlack.png",
   icon: "@/assets/icons/OSDGreen.png"
 };
